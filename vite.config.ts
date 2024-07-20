d6e35b26dac8c402e9720fa6740c2668a0d558c2
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-07-20 10:28:10 -0700

    proof of concept

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    babelTransformPlugin(),
     remix({
       future: {
         v3_fetcherPersist: true,
@@ -14,3 +15,36 @@ export default defineConfig({
     tsconfigPaths(),
   ],
 });
+
+function babelTransformPlugin(): Plugin {
+  return {
+    name: "babel-transform",
+    enforce: "pre",
+    transform(code, id) {
+      if (id.startsWith("\u0000virtual:")) {
+        return null;
+      }
+      if (id.includes("/node_modules/")) {
+        return null;
+      }
+      if (!/\.tsx?$/.test(id)) {
+        return null;
+      }
+
+      console.log("transforming", JSON.stringify(id));
+
+      // TODO: actually transform the code
+      const result = [
+        code,
+        "",
+        `console.log("transformed", ${JSON.stringify(id)});`,
+        "",
+      ].join("\n");
+
+      return {
+        code: result,
+        map: null,
+      };
+    },
+  };
+}
